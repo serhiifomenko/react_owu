131ef336933427c49e6290ff781812b447f7eb9f
Serhii Fomenko <sergeyfomenko622@gmail.com>
2026-09-18 03:26:55 +0300

    get settings react

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,9 +1,12 @@
 import {type FC} from "react";
+import './MyComponents.css'
 
 type MyComponentPropType = { text: string }
 
 const MyComponent: FC<MyComponentPropType> = ({text}) => {
-    return <div>{text}</div>
+    return <div className={'target'}>
+        {text}
+    </div>
 }
 
 export default MyComponent;
