300e8b6ce4f8127a70eaa4098fe0c3a6fcb45b3c
Serhii Fomenko <sergeyfomenko622@gmail.com>
2026-09-19 00:54:26 +0300

    react befour map

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './App.css'
+import './App.css';
 import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
-  return (
-    <>
-     <MyComponent text='Hello World!'/>
-      {
-        MyComponent({text: 'Hello3'})
-      }
-    </>
-  );
+
+    return (
+        <>
+            <MyComponent title={'hello1'}>
+                Lorem ipsum dolor sit.
+            </MyComponent>
+
+             <MyComponent title={'hello2'}></MyComponent>
+             <MyComponent title={'hello, okten!'}></MyComponent>
+             {/*MyComponent({text: 'hello 2'}) */}
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,12 +1,17 @@
-import {type FC} from "react";
-import './MyComponents.css'
+import type {FC, ReactNode} from 'react';
 
-type MyComponentPropType = { text: string }
+type MyComponentPropType = {
+    title: string;
+    children?: ReactNode;
+};
 
-const MyComponent: FC<MyComponentPropType> = ({text}) => {
-    return <div className={'target'}>
-        {text}
-    </div>
-}
+const MyComponent: FC<MyComponentPropType> = ({ title, children }) => {
+    return (
+        <div className="text-3xl font-bold underline">
+            <h2>{title}</h2>
+            <p>{children}</p>
+        </div>
+    );
+};
 
 export default MyComponent;
